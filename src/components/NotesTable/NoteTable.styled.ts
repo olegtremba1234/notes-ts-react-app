0a28e355d5e09b8ccc414e7bb62145d0bba0e99d
olegtremba1234 <olegtremba@gmail.com>
2023-08-02 23:02:00 +0300

    clean styled

--- src/components/NotesTable/NoteTable.styled.ts
+++ src/components/NotesTable/NoteTable.styled.ts
@@ -1,38 +1,11 @@
 import styled, { css } from 'styled-components';
 
 const noteTableBorderColor = '#ccc';
-const noteTableHeaderBg = '#f2f2f2';
 const noteTableRowBg = '#fff';
 
 interface TrProps {
   archived: boolean;
 }
-
-export const NoteTableContainer = styled.div`
-  width: 100%;
-  border-radius: 3px;
-  box-shadow: 0 0 10px rgba(118, 80, 13, 0.9);
-  overflow: hidden;
-  @media (max-width: 768px) {
-    overflow-x: scroll;
-  }
-`;
-
-export const NoteTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  border: 1px solid ${noteTableBorderColor};
-  color: rgb(0, 0, 24);
-
-  @media (max-width: 768px) {
-    font-size: 14px;
-
-    th,
-    td {
-      padding: 8px;
-    }
-  }
-`;
 
 export const Tr = styled.tr<TrProps>`
 ${({ archived }) =>
@@ -46,16 +19,6 @@ ${({ archived }) =>
   background-color: ${noteTableRowBg};
 `;
 
-export const Th = styled.th`
-  padding: 10px;
-  border-bottom: 1px solid ${noteTableBorderColor};
-  text-align: center;
-
-  @media (max-width: 768px) {
-    font-size: 12px;
-  }
-`;
-
 export const Td = styled.td`
   padding: 10px;
   border-bottom: 1px solid ${noteTableBorderColor};
@@ -64,11 +27,6 @@ export const Td = styled.td`
   @media (max-width: 768px) {
     font-size: 12px;
   }
-`;
-
-export const Thead = styled.thead`
-  background-color: ${noteTableHeaderBg};
-  font-weight: bold;
 `;
 
 export const ActionButton = styled.button`
--- src/components/SummaryTable/SummaryTable.styled.ts
+++ src/components/SummaryTable/SummaryTable.styled.ts
@@ -4,27 +4,6 @@ const noteTableBorderColor = '#ccc';
 const noteTableHeaderBg = '#f2f2f2';
 const noteTableRowBg = '#fff';
 
-export const TableContainer = styled.div`
-  margin-top: 50px;
-  border-radius: 3px;
-  box-shadow: 0 0 10px rgba(118, 80, 13, 0.9);
-  overflow: hidden;
-  /* overflow-x: scroll; */
-`;
-
-export const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-export const TableHeaderCell = styled.th`
-  padding: 10px;
-  border-bottom: 1px solid ${noteTableBorderColor};
-  text-align: center;
-  background-color: ${noteTableHeaderBg};
-  font-weight: bold;
-`;
-
 export const TableCell = styled.td`
   padding: 10px;
   border-bottom: 1px solid ${noteTableBorderColor};
