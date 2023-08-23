613f94022ce8f9202931555fae45b134ebba1263
olegtremba1234 <olegtremba@gmail.com>
2023-08-23 11:04:37 +0300

    upd createat date formatting
    now when u create note, u see only time, and when note have date older, than you see what day and month created at note

--- src/utils/timeConverter.tsx
+++ src/utils/timeConverter.tsx
@@ -9,16 +9,29 @@ const FormattedDate: React.FC<FormattedDateProps> = ({ date }) => {
     return null;
   }
 
-  const formattedDate = new Date(date).toLocaleString(undefined, {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
+  const now = new Date();
+  const formattedDate = new Date(date);
+
+  const isToday =
+    formattedDate.getDate() === now.getDate() &&
+    formattedDate.getMonth() === now.getMonth() &&
+    formattedDate.getFullYear() === now.getFullYear();
+
+  const options: Intl.DateTimeFormatOptions = {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-  });
+  };
 
-  return <span>{formattedDate}</span>;
+  if (!isToday) {
+    options.year = "numeric";
+    options.month = "short";
+    options.day = "numeric";
+  }
+
+  const formattedDateString = formattedDate.toLocaleString(undefined, options);
+
+  return <span>{formattedDateString}</span>;
 };
 
 export default FormattedDate;
